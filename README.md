b5a0739196c77530aa667aa3fd99f6a9a7e4734f
customwebtemplatedesigner <68409068+customwebtemplatedesigner@users.noreply.github.com>
2020-10-03 16:00:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# Reddit Bot Flair Update Command Bot
+This bot will follow a thread and __must have admin rights__ for setting suggested sort to new.\
+Its purpose is to have a thread on your own subreddit where users can submit commands to update their user flairs.
+
+
+## About <a name = "about"></a>
+
+This bot __must have admin rights__ on the sub for setting the suggested sort to new.\
+It will get the most recent top level comments from a specific thread which will be defined in the pw.env file.\
+Each time a new comment is found it will validate the command and will update the user flair accordingly.
+
+
+### Prerequisites
+
+This application runs on NodeJS. Make sure you have an updated version of node downloaded to fit your system.\
+You can find the download at https://nodejs.org/en/
+
+
+Now that you have Node.js installed, you must create an account for the bot.
+
+
+
+Once the account is created and you are logged into it, navigate to https://www.reddit.com/prefs/apps/
+
+
+Here you will tell reddit about your bot and get the values needed to authenticate against the API.
+
+1. click the [create app] button
+2. give it a name
+3. check the bubble for 'script'
+4. give it a description
+5. about url can be blank
+6. redirect uri can be http://www.google.com (this doesn't matter but it must be there and be a valid uri)
+7. Now that you have your script app created, you can use the values on this page to fill in your pw.env file
+
+<img src='https://i.imgur.com/yq8akJ7.png'>
+
+
+Before beginning the following steps, you must create a thread for the bot to follow and copy the thread id from the url bar.
+
+The post body should contain instructions on how to use the bot. For example:
+```
+To use this bot you must format a command like this:
+
+!flair <flair css class> <optional flair text>
+
+For example:
+
+!flair red my flair text
+
+This will result in your user flair being updated to a red background with text of "flair text"
+
+
+Try !flair help for a list of css class to choose from
+```
+
+To create new flairs to add to your bot go into your Mod Dashboard and under Flairs & Emojis click on User flair.\
+Create your flair and include any emojis you want in the Flair Text.\
+Now give it a unique css class. No spaces, only dashes.\
+Each of these css classes will need to be copy and pasted into your pw.env file.
+<img src='https://i.imgur.com/7OzENr5.png'>
+
+
+Navigate to pw.envEXAMPLE and remove EXAMPLE from the end of the filename.\
+Now fill it in with the data you found at reddit.com/prefs/apps\
+REDDIT_USER and REDDIT_PASS are the username and password of the bot account.\
+MASTER_SUB will be the subreddit the bot will work on. The bot __must have admin rights__ to set the suggested sort of this thread to new.\
+LIMIT defaults to 6 but can be set higher to check for missed requests while the bot was offline.\
+DEBUG_CODE will display information about the submission queue.\
+DEBUG_NETWORK will display all the http error codes received. After the initial startup, status code 200 is expected every 20 seconds.\
+THREAD_ID is found in the url of your submission. See the following url:\
+https://www.reddit.com/r/Bwz3rBot/comments/j4ispn/follow_this_post/\
+FLAIRS_LIST must be formatted as below:
+```
+USER_AGENT=''
+CLIENT_ID=''
+CLIENT_SECRET=''
+REDDIT_USER=''
+REDDIT_PASS=''
+MASTER_SUB=''
+DEBUG_CODE="false"
+DEBUG_NETWORK="false"
+THREAD_ID="j4596b"
+LIMIT="5"
+FLAIRS_LIST="blue,red,green,yellow,cake,dingus,coolio"
+```
+
+
+
+### Installing
+
+Download the source code from this page.
+
+Open up your terminal and cd into the folder of the files you just downloaded.
+
+Install dependencies:
+```
+npm i
+```
+
+Run the bot:
+```
+node src/app.js
+```
+
+### What now?
+
+Open up your favorite IDE and go into the BotService.js file.\
+This is where you will write all of your code.\
+You have access to the Snoowrap API here and a single comment from the defined thread.
---
+++ src/service/BotService.js
@@ -0,0 +1,102 @@
+const colors = require('colors');
+const Snoowrap = require('snoowrap');
+const requester = require('../config/snoo-config').actionRequester;
+const command = require('../util/command');
+const fl = process.env.FLAIRS_LIST;
+const flairsList = fl.split(',');
+
+// This is where your code will go. It must return a promise!
+// This function is called each time a new message is popped from the array.
+// It gives you a pre-fetched 'RedditObject' of type comment(mention).
+// The InboxStreamGenerator will always give you the first 5 mentions - unread or not
+// So be sure to be save-checking saving and every time!
+async function doSomething(item) {
+    // Checking if the item was saved will keep the bot from processing anything twice.
+    if (!item.saved) {
+        console.log(`BotService processing item: ${item.id}`.green)
+
+        //
+        // CODE STARTS HERE //
+
+        console.log(item.body.yellow)
+
+        let cmd = command.buildCMD(item.body);
+        if (cmd != null) {
+            console.log("assigning flairs...")
+            await assignFlairs(cmd, item);
+
+        } else {
+            console.log("replying with error message")
+            await requester.getComment(item.id).reply("Comment must be formatted as a command with prefix = !")
+        }
+
+
+        // CODE ENDS HERE //
+        //
+        // Be sure to finish with saveItem(item) so the item will not be processed again.
+        await saveItem(item) // Leave this line where it is!
+        return console.log(`item  ${item.id} successfully processed!`)
+    } else {
+        return console.log(`item ${item.id} saved already. Skipping...`)
+    }
+}
+
+// 
+// Write your functions down here and call them inside of doSomething.
+
+async function assignFlairs(cmd, item) {
+    console.log("command: ", cmd)
+    // Process the Command
+    if (cmd.directive == "flair") {
+        console.log("command directive was flair.".green)
+        let flairToAssign = cmd.args[0];
+        console.log("flair to assign: ", flairToAssign)
+        let foundFlair = flairsList.find(el => el == flairToAssign)
+
+        if (foundFlair == undefined) {
+            console.log("not found within the list. replying with error message.")
+            return requester.getComment(item.id).reply(`Sorry, but ${cmd.args[0]} does not exist within the list of flairs!\n\nTry ${fl}`)
+        } else {
+            console.log("Found flair within list:", foundFlair)
+            console.log("requester getting user by name: ", item.author.name)
+            console.log("assigning flair...")
+            let flairText = new String();
+            for (i = 1; i < cmd.args.length; i++) {
+                flairText = flairText + " " + cmd.args[i]
+            }
+            console.log("Flair text will be: ", flairText);
+
+            let defaultText;
+            console.log(`Getting user flair : "${cmd.args[0]}"`)
+            await requester.getSubreddit(process.env.MASTER_SUB).getUserFlairTemplates()
+                .then(templates => {
+                    let foundTemplate = templates.find(template => template.flair_css_class == cmd.args[0])
+                    console.log("Found this template: ", foundTemplate)
+                    defaultText = foundTemplate.flair_text
+                })
+            if (cmd.args.length >= 2) {
+                defaultText = flairText
+            }
+
+            return requester.getUser(item.author.name).assignFlair({
+                subredditName: process.env.MASTER_SUB,
+                text: defaultText.trim(),
+                cssClass: cmd.args[0],
+            })
+        }
+
+    } else {
+        console.log("command directive was NOT flair".red)
+    }
+
+}
+
+// 
+// Leave this function alone!
+const saveItem = function (item) {
+    console.log("saving the comment...".grey)
+    return requester.getComment(item.id).save();
+}
+module.exports = {
+    doSomething: doSomething
+}
